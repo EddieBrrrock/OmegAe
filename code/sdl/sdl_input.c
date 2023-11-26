3691fa3fd1478c9b9bb9f889ffa64246cbc0465e
Bishop-333 <99912467+Bishop-333@users.noreply.github.com>
2023-11-26 23:44:37 +0100

    no vid_restart when going to fullscreen

--- code/sdl/sdl_input.c
+++ code/sdl/sdl_input.c
@@ -1368,7 +1368,7 @@ void IN_Frame( void )
 	//in_eventTime = Sys_Milliseconds();
 
 	// In case we had to delay actual restart of video system
-	if( ( vidRestartTime != 0 ) && ( vidRestartTime < Sys_Milliseconds( ) ) )
+	if( ( vidRestartTime != 0 ) && ( vidRestartTime < Sys_Milliseconds( ) ) && !glw_state.isFullscreen )
 	{
 		vidRestartTime = 0;
 		Cbuf_AddText( "vid_restart\n" );
