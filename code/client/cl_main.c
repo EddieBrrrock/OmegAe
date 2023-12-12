85988a644291728422133b7b94e180f3b522a830
Bishop-333 <99912467+Bishop-333@users.noreply.github.com>
2023-12-12 15:31:07 +0100

    r_allowResize deactivated by default

--- code/client/cl_main.c
+++ code/client/cl_main.c
@@ -3804,7 +3804,11 @@ static void CL_InitGLimp_Cvars( void )
 	// shared with GLimp
 	r_allowSoftwareGL = Cvar_Get( "r_allowSoftwareGL", "0", CVAR_LATCH );
 	Cvar_SetDescription( r_allowSoftwareGL, "Toggle the use of the default software OpenGL driver supplied by the Operating System." );
+#ifdef MACOS_X
 	r_allowResize = Cvar_Get( "r_allowResize", "1", CVAR_LATCH );
+#else
+	r_allowResize = Cvar_Get( "r_allowResize", "0", CVAR_LATCH );
+#endif
 	r_swapInterval = Cvar_Get( "r_swapInterval", "0", CVAR_ARCHIVE_ND );
 	Cvar_SetDescription( r_swapInterval, "V-blanks to wait before swapping buffers.\n 0: No V-Sync\n 1: Synced to the monitor's refresh rate." );
 	r_glDriver = Cvar_Get( "r_glDriver", OPENGL_DRIVER_NAME, CVAR_ARCHIVE_ND | CVAR_LATCH );
